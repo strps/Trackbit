795cb355e74b250331653fd7bbcd6ad528a0fe7c
strps <csrstrps@gmail.com>
2026-01-01 23:08:14 +0000

    Fixing migrations minoe issue

--- apps/backend/drizzle/0002_great_genesis.sql
+++ apps/backend/drizzle/0002_great_genesis.sql
@@ -7,7 +7,7 @@ ALTER TABLE "exercise_sets" RENAME TO "exercise_performances";--> statement-brea
 ALTER TABLE "exercise_performances" DROP CONSTRAINT "exercise_sets_exercise_log_id_exercise_log_id_fk";
 --> statement-breakpoint
 ALTER TABLE "habits" ALTER COLUMN "color_palette" SET DEFAULT '[{"position":0,"color":[255,0,0,1]},{"position":0.5,"color":[255,225,0,1]},{"position":1,"color":[12,148,62,1]}]'::jsonb;--> statement-breakpoint
-ALTER TABLE "exercise_performances" ADD COLUMN "number" integer NOT NULL SET DEFAULT 1;--> statement-breakpoint
+ALTER TABLE "exercise_performances" ADD COLUMN "number" integer DEFAULT 1 NOT NULL;--> statement-breakpoint
 ALTER TABLE "exercise_performances" ADD COLUMN "duration_miliseconds" integer;--> statement-breakpoint
 ALTER TABLE "exercise_performances" ADD COLUMN "distance" numeric;--> statement-breakpoint
 ALTER TABLE "habits" ADD COLUMN "color_theme" "color_scale" DEFAULT 'green' NOT NULL;--> statement-breakpoint
