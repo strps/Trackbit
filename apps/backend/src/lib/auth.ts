9c42c5ce49efcdb49687e0b180b0d2a0f648c177
strps <csrstrps@gmail.com>
2026-01-02 02:08:57 +0000

    m

--- apps/backend/src/lib/auth.ts
+++ apps/backend/src/lib/auth.ts
@@ -61,12 +61,10 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-      redirectURI: `${process.env.FRONT_URL}/dashboard`
     },
     github: {
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
-      redirectURI: `${process.env.FRONT_URL}/dashboard`
     },
   },
 
